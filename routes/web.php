e4971256d802b7ddd746e7f8ecaa7c5f7ed754a5
karmakatoro <karmakatoro@gmail.com>
2023-03-22 10:16:20 +0200

    set registration route as welcome

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.registration');
 });
+
+Route::get('registration', [CustomAuthController::class, 'registration'])
+->name('register');
