9458b881e45ddc0a075bf7822f6b47872c35ba34
karmakatoro <karmakatoro@gmail.com>
2023-03-22 12:54:44 +0200

    create profile routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,20 +19,18 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('registration', [CustomAuthController::class, 'registration'])
-->name('register');
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register');
 
-Route::post('custom-registration',[CustomAuthController::class, 'custom_registration'])
-->name('register.custom');
+Route::post('custom-registration',[CustomAuthController::class, 'custom_registration'])->name('register.custom');
 
-Route::get('login', [CustomAuthController::class, 'index'])
-->name('login');
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 
-Route::post('custom-login', [CustomAuthController::class, 'custom_login'])
-->name('login.custom');
+Route::post('custom-login', [CustomAuthController::class, 'custom_login'])->name('login.custom');
 
-Route::get('dashboard', [CustomAuthController::class, 'dashboard'])
-->name('dashboard');
+Route::get('dashboard', [CustomAuthController::class, 'dashboard'])->name('dashboard');
 
-Route::get('logout', [CustomAuthController::class, 'logout'])
-->name('logout');
+Route::get('logout', [CustomAuthController::class, 'logout'])->name('logout');
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
+Route::post('profile/edit_validation', [ProfileController::class, 'edit_validation'])->name('profile.edit_validation');
