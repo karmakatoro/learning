2268856654712089dd8b9f875532a18126dad48d
karmakatoro <karmakatoro@gmail.com>
2023-03-22 10:55:18 +0200

    create registration form

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -5,21 +5,35 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-4">
+                @if(session()->has('success'))
+                    <div class="alert alert-success">
+                        {{ session()->get('success') }}
+                    </div>
+                @endif
                 <div class="card">
                     <h3 class="card-header text-center">
                         Register User
                     </h3>
                     <div class="card-body">
-                        <form action="" method="post">
+                        <form action="{{ route('register.custom') }}" method="post">
                            @csrf
                             <div class="form-group mb-3">
                                 <input type="text" name="name" id="" class="form-control" placeholder="Name">
+                                @if($errors->has('name'))
+                                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                                @endif
                             </div>
                             <div class="form-group mb-3">
                                 <input type="email" name="email" id="" class="form-control" placeholder="Email">
+                                @if($errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                @endif
                             </div>
                             <div class="form-group mb-3">
                                 <input type="password" name="password" id="" class="form-control" placeholder="Passwod">
+                                @if($errors->has('password'))
+                                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                                @endif
                             </div>
                             <div class="d-grid mx-auto">
                                 <button type="submit" class="btn btn-dark btn-block">Sign Up</button>
