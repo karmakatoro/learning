599babdbddccc619897090e31a9ba336029cc7cb
karmakatoro <karmakatoro@gmail.com>
2023-03-28 12:14:54 +0200

    updates

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <title>Visitor</title>
+</head>
+<body>
+
+    @yield('content')
+
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.6.3.min.js') }}"></script>
+</body>
+</html>
